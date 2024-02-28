a2a5b0a306e32cc41f194afff130cf54c6bc6422
Alessandro Pinto <alessandropinto2003@gmail.com>
2024-02-28 15:41:10 +0100

    Login Redirect fixed Hello Redirect fixed

--- src/main/java/com/watchers/HelloServlet.java
+++ src/main/java/com/watchers/HelloServlet.java
@@ -1,6 +1,10 @@
 package com.watchers;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -16,12 +20,19 @@ public class HelloServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
+        response.encodeRedirectURL("\"C:\\\\Users\\\\aless\\\\OneDrive\\\\Documenti\\\\Università\\\\Web\\\\src\\\\main\\\\webapp\\\\WEB-INF\\\\test.jsp\"");
 
         // Hello
         PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+
+        List<String> lines = Files.readAllLines(Path.of("C:\\Users\\aless\\OneDrive\\Documenti\\Università\\Web\\src\\main\\webapp\\WEB-INF\\test.jsp"));
+        for(String line: lines){
+            //out.println(line);
+        }
+        out.flush();
+        //out.println("<html><body>");
+        //out.println("<h1>" + message + "</h1>");
+        //out.println("</body></html>");
     }
 
     public void destroy() {
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,7 +1,17 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
 <html>
+<head>
+    <title>JSP - Hello World</title>
+</head>
 <body>
 <h2>Hello World!</h2>
-<a href="helloServlet">hello</a>
-<a href="loginServlet">login</a>
+<a href="hello-servlet">hello</a>
+<a href="login-servlet">login</a>
+<form id = "name" action="hello-servlet" method="get">
+    <label>Nome:</label><br>
+    <input type="text">
+    <input type="submit">
+</form>
 </body>
 </html>
