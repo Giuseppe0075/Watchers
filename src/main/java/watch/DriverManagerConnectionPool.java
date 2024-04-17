d8d5fe730e7a431e8973ed39e29b946be0e62c63
Giuseppe <giuseppesica03@gmail.com>
2024-04-17 16:31:19 +0200

    Creato il file DriverManagerConnectionPool.java. Aggiunto al .gitignore il file dataSources.xml

---
+++ src/main/java/watch/DriverManagerConnectionPool.java
@@ -0,0 +1,48 @@
+package watch;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DriverManagerConnectionPool {
+    private static List<Connection> freeDbConnections;
+
+    static {
+        freeDbConnections = new LinkedList<Connection>();
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("DB driver not found!");
+        }
+    }
+
+    private static Connection createDBConnection() throws SQLException {
+        Connection newConnection = null;
+        String db = "mioDb";
+        String username = "login";
+        String password = "password";
+        newConnection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/"+db, username, password);
+        newConnection.setAutoCommit(false);
+        return newConnection;
+    }
+    public static synchronized Connection getConnection() throws SQLException {
+        Connection connection;
+        if (! freeDbConnections.isEmpty()) {
+            connection = (Connection) freeDbConnections.get(0);
+            DriverManagerConnectionPool.freeDbConnections.remove(0);
+            try {
+                if (connection.isClosed())
+                    connection = DriverManagerConnectionPool.getConnection();
+            } catch (SQLException e) {
+                connection = DriverManagerConnectionPool.getConnection();
+            }
+        } else connection = DriverManagerConnectionPool.createDBConnection();
+        return connection;
+    }
+    public static synchronized void releaseConnection(Connection connection) {
+        DriverManagerConnectionPool.freeDbConnections.add(connection);
+    }
+}
