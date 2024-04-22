6a051e7aafdfc38840e88b013ba6f27a5ae59cdf
Alessandro Pinto <alessandropinto2003@gmail.com>
2024-04-22 14:50:22 +0200

    Sanitaizer class

--- src/main/java/utils/Sanitazer.java
+++ src/main/java/utils/Sanitazer.java
@@ -1,5 +1,9 @@
 package utils;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
 public class Sanitazer {
     public static String sanitizeInput(String input){
         if (input == null) {
@@ -12,5 +16,16 @@ public class Sanitazer {
                 .replaceAll("\"", "&quot;")
                 .replaceAll("'", "&#x27;");
         return input;
+    }
+
+
+    public static String getCSRFToken() throws NoSuchAlgorithmException {
+        // generate random data
+        SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+        byte[] data = new byte[16];
+        secureRandom.nextBytes(data);
+
+        // convert to Base64 string
+        return Base64.getEncoder().encodeToString(data);
     }
 }
